e276a7f72c141200a0cd3706043f799fc0135447
user <user@gmail.com>
2023-05-16 16:22:21 +0300

    1. Two Sum

---
+++ index.js
@@ -0,0 +1,18 @@
+// 1. Two Sum
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let result = []
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        result.push(i, j)
+        return result
+      }
+    }
+  }
+}
+console.log(twoSum([3, 2, 3], 6)) // Expected [0,2]
