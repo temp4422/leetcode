443c6c77caff1282e3fbe1172663687a63573b37
user <user@gmail.com>
2023-06-19 20:42:29 +0300

    WIP 380. Insert Delete GetRandom O(1), Medium

--- index.js
+++ index.js
@@ -606,4 +606,90 @@ var hIndex = function (citations) {
 
   return res
 }
-console.log(hIndex(citations))
+// console.log(hIndex(citations))
+
+// 380. Insert Delete GetRandom O(1), Medium
+// Array, Hash Table, Math, Design, Randomized
+/*****************************************************************************/
+/*
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+*/
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+// TODO time complexity O(1)
+// TODO Create shadow array with elements and swap deleted element with last one
+const RandomizedSet = function () {
+  this.map = new Map()
+  this.arr = []
+}
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+  // If value exsists return false, else set new value
+  if (this.map.has(val)) {
+    return false
+  } else {
+    this.map.set(val, this.arr.length)
+    this.arr.push(val)
+    return true
+  }
+}
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+  if (this.map.has(val)) {
+    // Get index of element to remove -> same as array index
+    let index = this.map.get(val)
+    // Change element on this index with last element of array, i.e. reassign element at index with element at last index
+    this.arr[index] = this.arr.at(-1)
+    // Remove last element from array, because it's already on the new index
+    this.arr.pop()
+    // Reset map with reassigned element (change index of this element)
+    this.map.set(this.arr[index], index)
+    // Delete element from map
+    this.map.delete(val)
+    return true
+  } else {
+    return false
+  }
+}
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+  let randomIndex = Math.floor(Math.random() * this.arr.length)
+  return this.arr.at(randomIndex)
+}
+// Test cases
+const randomizedSet = new RandomizedSet() //?
+randomizedSet.insert(1) //?
+randomizedSet.remove(2) //?
+randomizedSet.insert(2) //?
+randomizedSet.getRandom() //?
+randomizedSet.remove(1) //?
+randomizedSet.insert(2) //?
+randomizedSet.getRandom() //?
+console.log(randomizedSet.map)
