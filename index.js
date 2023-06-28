6c332ae4b5b4c89a98c257f46c651280a386245d
user <user@gmail.com>
2023-06-28 20:34:13 +0300

    1306. Jump Game III, Medium, 3days+, with: BFS, DFS, DFS-Recursive 🔴 💀

--- index.js
+++ index.js
@@ -20,6 +20,115 @@ var twoSum = function (nums, target) {
 }
 // console.log(twoSum([3, 2, 3], 6)) // Expected [0,2]
 
+// 1306. Jump Game III, Medium
+// Array, Depth-first search, Breadth-first search
+/*****************************************************************************/
+/*
+Positioned at start index of the array, check if you can reach to any index with value 0 ?
+TODO: Use DFS for undirected unweighted acyclic graph to find target.
+Steps:
+1. build graph with all indexes (hash map)
+[4, 2, 3, 0, 3, 1, 2] values
+[0, 1, 2, 3, 4, 5, 6] indicesk
+Map { 0 => [ 4, null ], each record corespond to jump indicies: jump from index 0 to index -4(null) and 4
+      1 => [ 3, null ],
+      2 => [ 5, null ],
+      3 => [ 3, 3 ],
+      4 => [ null, 1 ],
+      5 => [ 6, 4 ],
+      6 => [ null, 4 ] }
+2. traverse the graph to find if it containt target
+*/
+// ;(arr = [4, 2, 3, 0, 3, 1, 2]), (start = 5) // Output: true
+// ;(arr = [4, 2, 3, 0, 3, 1, 2]), (start = 0) // Output: true
+// ;(arr = [3, 0, 2, 1, 2]), (start = 2) // Output: false
+// ;(arr = [0, 1]), (start = 1) // true
+// ;(arr = [47, 26, 216, 78, 179, 101, 42, 233, 185, 56, 303, 310, 169, 338, 51, 104, 308, 162, 81, 82, 169,41, 106, 150, 285, 298, 33, 251, 289, 236, 256, 227, 197, 186, 267, 326, 268, 243, 89, 347, 72, 0,89, 157, 90, 333, 327, 76, 106, 68, 355, 124, 234, 70, 43, 248, 259, 280, 199, 201, 312, 327, 217,278, 330, 258, 348, 351, 223, 240, 143, 244, 64, 343, 339, 101, 193, 18, 140, 312, 71, 225, 111,79, 199, 226, 321, 344, 31, 177, 362, 115, 341, 79, 146, 303, 348, 291, 250, 169, 78, 307, 325,33, 338, 316, 201, 343, 37, 37, 0, 15, 341, 38, 44, 67, 280, 128, 31, 106, 220, 172, 349, 142,339, 181, 102, 351, 81, 209, 41, 181, 59, 216, 230, 170, 257, 52, 5, 338, 28, 75, 208, 307, 108,103, 34, 342, 82, 233, 263, 12, 167, 358, 316, 150, 337, 158, 78, 231, 26, 22, 147, 81, 12, 319,161, 12, 75, 129, 54, 119, 131, 334, 292, 253, 255, 98, 39, 67, 146, 15, 329, 120, 80, 347, 89,124, 303, 315, 235, 55, 1, 100, 290, 187, 333, 326, 87, 138, 48, 41, 153, 118, 192, 152, 279, 69,154, 71, 152, 273, 61, 153, 267, 51, 106, 225, 204, 327, 50, 15, 202, 244, 328, 3, 150, 355, 240,240, 188, 92, 107, 244, 280, 102, 265, 273, 328, 115, 70, 221, 357, 101, 186, 251, 116, 24, 125,58, 185, 34, 356, 21, 108, 221, 169, 208, 230, 226, 235, 336, 304, 315, 334, 329, 229, 190, 20,104, 348, 132, 66, 265, 55, 212, 102, 167, 52, 2, 328, 114, 101, 196, 99, 155, 158, 337, 191, 119,14, 347, 127, 305, 142, 156, 92, 340, 358, 58, 7, 178, 79, 355, 289, 199, 251, 233, 351, 57, 115,306, 179, 31, 42, 123, 87, 101, 218, 71, 193, 205, 300, 180, 42, 19, 280, 233, 293, 181, 147, 359,190, 168, 191, 5, 58, 198, 154, 139, 29, 342, 261, 245, 141, 26, 251, 162, 360, 219, 233, 297,287, 262, 112, 87, 261, 21, 205, 131, 98, 161, 103, 57,]), (start = 313) // true
+// ;(arr = [4, 2, 3, 0, 3, 1, 2]), (start = 5) // true
+// ;(arr = [4, 4, 1, 3, 0, 3]), (start = 2) // true
+// ;(arr = [5, 11, 18, 16, 21, 3, 19, 0, 16, 4, 9, 20, 2, 13, 0, 2, 23, 8, 19, 22, 16, 19, 19, 25, 25, 15, 7,]),(start = 18) // false
+// ;(arr = [1, 1, 1, 1, 1, 1, 1, 1, 0]), (start = 3) // true
+// ;(arr = [4, 2, 3, 0, 3, 1, 2]), (start = 0) // true
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+const canReach = function (arr, start) {
+  // 1. Build graph as hash map
+  const graph = new Map()
+  function buildGraph() {
+    // 1.1 Check if index lower/bigger than arr, if so return empty values
+    function check(item) {
+      let left = item - arr[item]
+      let right = item + arr[item]
+      if (left == right) return [item] // Target
+      if (left < 0 && right > arr.length - 1) return [] // Empty
+      if (left < 0) return [right] // Only right
+      if (right > arr.length - 1) return [left] // Only left
+      return [left, right] // Both values valid
+    }
+    // 1.2 Set each node in graph (index) to adjecent nodes (indices) = i [i + arr.at(i), i - arr.at(i)]
+    // i.e. Make nodes and conections to other nodes
+    for (let i = 0; i < arr.length; i++) {
+      graph.set(i, check(i))
+    }
+  }
+  buildGraph()
+  // console.log(graph)
+
+  // 2. BFS, DFS, DFS-Recursive
+  // Breadth-first search on grpah
+  function bfs() {
+    const visited = new Set()
+    const stack = [start]
+    for (let i = 0; i < stack.length; i++) {
+      const idx = stack[i]
+      if (arr[idx] === 0) return true
+      if (!visited.has(idx)) {
+        visited.add(idx)
+        stack.push(...graph.get(idx))
+      }
+    }
+    return false
+  }
+  // return bfs()
+
+  // Depth-first search on graph
+  function dfs() {
+    const visited = new Set()
+    const stack = [start]
+    while (stack.length > 0) {
+      const idx = stack.pop() // Current index = last item in stack
+      if (arr[idx] === 0) return true // Found target
+      if (!visited.has(idx)) {
+        visited.add(idx) // Add current item to visited, so won't execute same item later
+        stack.push(...graph.get(idx)) // Get current node neighbors into stack
+      }
+    }
+    return false
+  }
+  // return dfs()
+
+  // Depth-first search recursive on graph
+  const visited = new Set()
+  const stack = []
+  function dfsRecursive() {
+    if (arr[start] === 0) return true
+    visited.add(start)
+    stack.push(...graph.get(start)) // Add adjacent nodes to stack
+    for (let i = stack.length - 1; i >= 0; i--) {
+      // Cycle from end to start
+      start = stack[i]
+      // 'arr' and 'start' taken from outer function, while 'visited' and 'stack' should be declared inside recursive function manullay. So this: 'recursive(visited, stack)' - also valid.
+      if (!visited.has(start)) return dfsRecursive(arr, start, visited, stack)
+    }
+    return false
+  }
+  // return dfsRecursive()
+}
+// console.log(canReach(arr, start))
+
 /*****************************************************************************/
 /********************* TOP INTERVIEW 150  ************************************/
 /*****************************************************************************/
@@ -37,7 +146,7 @@ var twoSum = function (nums, target) {
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-;(nums1 = [1, 2, 3, 0, 0, 0]), (m = 3), (nums2 = [2, 5, 6]), (n = 3) // Expected [1,2,2,3,5,6]
+// ;(nums1 = [1, 2, 3, 0, 0, 0]), (m = 3), (nums2 = [2, 5, 6]), (n = 3) // Expected [1,2,2,3,5,6]
 // ;(nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0]), (m = 6), (nums2 = [1, 2, 2]), (n = 3) // Expected [-1,0,0,1,2,2,3,3,3]
 /*nums1 =[-10,-10,-9,-9,-9,-8,-8,-7,-7,-7,-6,-6,-6,-6,-6,-6,-6,-5,-5,-5,-4,-4,-4,-3,-3,-2,-2,-1,-1,0,1,1,1,2,2,2,3,3,3,4,5,5,6,6,6,6,7,7,7,7,8,9,9,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 nums2 = [-10,-10,-9,-9,-9,-9,-8,-8,-8,-8,-8,-7,-7,-7,-7,-7,-7,-7,-7,-6,-6,-6,-6,-5,-5,-5,-5,-5,-4,-4,-4,-4,-4,-3,-3,-3,-2,-2,-2,-2,-2,-2,-2,-1,-1,-1,0,0,0,0,0,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,9,9,9,9]
