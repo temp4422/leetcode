af9889077d5256a25d5b56a00f4133952f8678e5
user <user@MacBook-Pro-user.local>
2023-09-16 09:58:50 +0300

    2626. Array Reduce Transformation, Easy, 00:10 🟢

--- index.js
+++ index.js
@@ -240,3 +240,32 @@ var map = function (arr, fn) {
   return arrX
 }
 // console.log(map(arr, fn))
+
+// 2626. Array Reduce Transformation, Easy
+//prettier-ignore
+// nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr; }, init = 0 //Output: 10
+//prettier-ignore
+// nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr * curr; }, init = 100 //Output: 130
+//prettier-ignore
+// nums = [], fn = function sum(accum, curr) { return 0; }, init = 25 //Output: 25
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  if (nums.length === 0) {
+    return init
+  }
+
+  let arrX = []
+  let tmp = init
+
+  for (let i = 0; i < nums.length; i++) {
+    tmp = fn(tmp, nums[i])
+    arrX.push(tmp)
+  }
+  return arrX[arrX.length - 1]
+}
+// console.log(reduce(nums, fn, init))
