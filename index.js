f08ec6ec08c4860c6b0d54497b3c0ebb913cb509
user <user@MacBook-Pro-user.local>
2023-11-16 10:18:39 +0200

    2724. Sort By, Easy, 00:20, 🟢

--- index.js
+++ index.js
@@ -573,3 +573,18 @@ var addTwoPromises = async function (promise1, promise2) {
 }
 // addTwoPromises(promise1, promise2) // 4
 // addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log) // 4
+
+// 2724. Sort By, Easy
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+  const sorted = arr.sort((a, b) => fn(a) - fn(b))
+  return sorted
+}
+// console.log(sortBy(arr, fn))
+// arr = [5, 4, 1, 2, 3], fn = (x) => x // Output: [1, 2, 3, 4, 5]
+// arr = [{"x": 1}, {"x": 0}, {"x": -1}], fn = (d) => d.x // Output: [{"x": -1}, {"x": 0}, {"x": 1}]
+// arr = [[3, 4], [5, 2], [10, 1]], fn = (x) => x[1] // Output: [[10, 1], [5, 2], [3, 4]]
