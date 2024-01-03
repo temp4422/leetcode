8515f1af6816ff13d6d76f7ec17793a09b2638b6
user <user@MacBook-Pro-user.local>
2024-01-03 09:33:36 +0200

    191. Number of 1 Bits, Easy, 00:10, 🟢

--- top-interview-150.js
+++ top-interview-150.js
@@ -2616,6 +2616,24 @@ var reverseBits = function (n) {
 // # 191. Number of 1 Bits, Easy
 // Divide and Conquer, Bit Manipulation
 /*****************************************************************************/
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let str = n.toString(2)
+  let count = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == 1) count++
+  }
+
+  return count
+}
+// testFunction = hammingWeight
+// input(11).output(3) //?
+// input(128).output(1) //?
+// input(4294967293).output(31) //?
 
 // # 136. Single Number, Easy
 // Array, Bit Manipulation
