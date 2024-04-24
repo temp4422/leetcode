65ce6104448d2508a76a870ffed5a1f74875f1b3
user <user@MacBook-Pro-user.local>
2024-04-24 17:22:09 +0300

    303. Range Sum Query - Immutable, Easy, 00:10, 🟢

--- index.js
+++ index.js
@@ -923,3 +923,41 @@ var solution = function (isBadVersion) {
 // }
 // const testBadVersion = solution(isBadVersion)
 // testBadVersion(5)
+
+// 303. Range Sum Query - Immutable, Easy
+// /**
+//  * @param {number[]} nums
+//  */
+// var NumArray = function (nums) {
+//   this.nums = nums
+// }
+// /**
+//  * @param {number} left
+//  * @param {number} right
+//  * @return {number}
+//  */
+// NumArray.prototype.sumRange = function (left, right) {
+//   const cut = this.nums.slice(left, right + 1)
+//   const sum = cut.reduce((a, b) => a + b)
+//   return sum
+// }
+// /**
+//  * Your NumArray object will be instantiated and called as such:
+//  * var obj = new NumArray(nums)
+//  * var param_1 = obj.sumRange(left,right)
+//  */
+// Aletrnative
+class NumArray {
+  constructor(nums) {
+    this.nums = nums
+  }
+  sumRange(left, right) {
+    const cut = this.nums.slice(left, right + 1)
+    const sum = cut.reduce((a, b) => a + b)
+    return sum
+  }
+}
+// const numArray = new NumArray([-2, 0, 3, -5, 2, -1])
+// numArray.sumRange(0, 2) // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5) // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5) // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
