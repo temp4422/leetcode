a949b7753b29f0c88ef85aa93b0cb3944ba588f3
user <user@MacBook-Pro-user.local>
2024-07-13 08:01:57 +0300

    804. Unique Morse Code Words, Easy, 00:15, 🟢

--- index.js
+++ index.js
@@ -2053,3 +2053,26 @@ class MyHashMap {
 // obj.put('test', 111)
 // obj.get('test')
 // obj.remove('test')
+
+// 804. Unique Morse Code Words, Easy
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+  //prettier-ignore
+  const morseCodeTable = {a:'.-',b:'-...',c:'-.-.',d:'-..',e:'.',f:'..-.',g:'--.',h:'....',i:'..',j:'.---',k:'-.-',l:'.-..',m:'--',n:'-.',o:'---',p:'.--.',q:'--.-',r:'.-.',s:'...',t:'-',u:'..-',v:'...-',w:'.--',x:'-..-',y:'-.--',z:'--..'}
+  let transformation = ''
+  let transformationArray = []
+
+  for (const word of words) {
+    for (let i = 0; i < word.length; i++) {
+      transformation += morseCodeTable[word[i]]
+    }
+    transformationArray.push(transformation)
+    transformation = ''
+  }
+
+  return new Set(transformationArray).size
+}
+// testFunction(uniqueMorseRepresentations).input(['gin', 'zen', 'gig', 'msg']).output(2) //?
