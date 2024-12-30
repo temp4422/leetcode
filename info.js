38c6571eae532216fde7bfcd4d9427258fb2e5ed
user <user@MacBook-Pro-user.local>
2024-12-30 10:19:38 +0200

    Refactor info.js

--- info.js
+++ info.js
@@ -1,24 +1,24 @@
 // Alternative map expressions
 const map = new Map()
-const nums = ['a', 'b', 'c', 'c', 'c']
+const chars = ['a', 'b', 'c', 'c', 'c']
 
-for (const num of nums) {
+for (const char of chars) {
   // Logical OR operator, misbehave with '0'
-  // map.set(num, map.get(num) + 1 || 1)
+  // map.set(char, map.get(char) + 1 || 1)
 
   // Logical OR operator, handle '0'
-  // map.set(num, (map.get(num) || 0) + 1)
+  // map.set(char, (map.get(char) || 0) + 1)
 
   // Nullish coalescing operator, handle '0' ⭐️
-  map.set(num, (map.get(num) ?? 0) + 1)
+  map.set(char, (map.get(char) ?? 0) + 1)
 
   // Ternary operator, handle '0'
-  // map.set(num, map.has(num) ? map.get(num) + 1 : 1)
+  // map.set(char, map.has(char) ? map.get(char) + 1 : 1)
 
   // if...else conditions, misbehave with '0'
-  // if (map.get(num)) {
-  //   map.set(num, map.get(num) + 1)
+  // if (map.get(char)) {
+  //   map.set(char, map.get(char) + 1)
   // } else {
-  //   map.set(num, 1)
+  //   map.set(char, 1)
   // }
 }
