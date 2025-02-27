2256a0145b9552a64671e43199ae143e0bf6a515
user <user@MacBook-Pro-user.local>
2025-02-27 14:45:31 +0200

    Refactor

--- _snippets.js
+++ _snippets.js
@@ -3,19 +3,19 @@ const map = new Map()
 const chars = ['a', 'b', 'c', 'c', 'c']
 
 for (const char of chars) {
-  // Logical OR operator, misbehave with '0'
+  // 1. Logical OR operator, misbehave with '0'
   // map.set(char, map.get(char) + 1 || 1)
 
-  // Logical OR operator, handle '0'
+  // 2. Logical OR operator, handle '0'
   // map.set(char, (map.get(char) || 0) + 1)
 
-  // Nullish coalescing operator, handle '0' ⭐️
+  // 3. Nullish coalescing operator, handle '0' ⭐️
   map.set(char, (map.get(char) ?? 0) + 1)
 
-  // Ternary operator, handle '0'
+  // 4. Ternary operator, handle '0'
   // map.set(char, map.has(char) ? map.get(char) + 1 : 1)
 
-  // if...else conditions, misbehave with '0'
+  // 5. if...else conditions, misbehave with '0'
   // if (map.get(char)) {
   //   map.set(char, map.get(char) + 1)
   // } else {
@@ -23,9 +23,9 @@ for (const char of chars) {
   // }
 }
 
-// Sort map by value [['a', 1], ['b', 2] ['c', 3]]
+// Sort map by value [['a', 1], ['b', 1] ['c', 3]]
 const sortedMap = [...map.entries()].sort((a, b) => a[1] - b[1])
 
 // Get key or value at some index
-let key = [...sortedMap.keys()].at(0)
-let value = [...sortedMap.values()].at(0)
+const key = [...sortedMap.keys()].at(0)
+const value = [...sortedMap.values()].at(0)
