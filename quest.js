acb6b16a43de21fc345a68e4634817edd17dcf5e
user <user@MacBook-Pro-user.local>
2026-01-19 15:04:50 +0200

    Q2. Shuffle the Array, Easy, 00:05, 🟢

--- quest.js
+++ quest.js
@@ -11,3 +11,18 @@ var getConcatenation = function (nums) {
   return nums.concat(nums)
 }
 // testFunction(getConcatenation).input([1, 2, 1]).output([1, 2, 1, 1, 2, 1]) //?
+
+// Q2. Shuffle the Array, Easy
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  const newArr = []
+  for (let i = 0; i < n; i++) {
+    newArr.push(nums[i], nums[i + n])
+  }
+  return newArr
+}
+// testFunction(shuffle).input([2, 5, 1, 3, 4, 7], 3).output([2, 3, 5, 4, 1, 7]) //?
